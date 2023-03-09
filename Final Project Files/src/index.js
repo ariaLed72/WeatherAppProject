22802d8c937ed5e7cb38a582080970f8fd7ac439
Ariana Ledezma <127069106+ariaLed72@users.noreply.github.com>
2023-03-08 20:27:18 -0800

    Working JS for Current Location Button
    No error messages

--- Final Project Files/src/index.js
+++ Final Project Files/src/index.js
@@ -37,14 +37,7 @@ function search(event) {
   changeCity.textContent = `${searchInput.value}`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=imperial`;
   searchInput.value = "";
-  axios.get(apiUrl).then((response) => {
-    changeDescription(response);
-    changeHumidity(response);
-
-    changeFeelsLike(response);
-    changeWind(response);
-    changeTemp(response);
-  });
+  axios.get(apiUrl).then(changeTemp);
 }
 
 let form = document.querySelector("form");
@@ -55,45 +48,29 @@ function changeTemp(response) {
   let wholeTemp = document.querySelector("#big-temp");
   let temperature = Math.round(response.data.main.temp);
   wholeTemp.innerHTML = `${temperature}`;
+  console.log(response);
+
+  let weatherPattern = document.querySelector("#description");
+  let description = response.data.weather[0].description;
+  weatherPattern.innerHTML = `${description}`;
+
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.main.humidity}`;
+
+  let feelsLike = document.querySelector("#feels-like");
+  let feelsLikeRound = Math.round(response.data.main.feels_like);
+  feelsLike.innerHTML = `${feelsLikeRound}`;
+
+  let wind = document.querySelector("#wind");
+  let windSpeed = Math.round(response.data.wind.speed);
+  wind.innerHTML = `${windSpeed}`;
 }
 
 function showPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiUrlPosition = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-  axios
-    .get(apiUrlPosition)
-    .then(changeTemp)
-    .then(changeDescription)
-    .then(changeHumidity)
-    .then(changeFeelsLike)
-    .then(changeWind);
-}
-
-function changeDescription(response) {
-  console.log(response.data.weather[0].description);
-  let weatherPattern = document.querySelector("#weather-pattern");
-  let description = response.data.weather[0].description;
-  weatherPattern.innerHTML = `${description}`;
-}
-
-function changeHumidity(response) {
-  console.log(response.data.main.humidity);
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `${response.data.main.humidity}`;
-}
-
-function changeFeelsLike(response) {
-  console.log(response.data.main.feels_like);
-  let feelsLike = document.querySelector("#feels-like");
-  feelsLike.innerHTML = `${response.data.main.feels_like}`;
-}
-
-function changeWind(response) {
-  console.log(response.data.wind.speed);
-  let wind = document.querySelector("#wind");
-  let windSpeed = Math.round(response.data.wind.speed);
-  wind.innerHTML = `${windSpeed}`;
+  axios.get(apiUrlPosition).then(changeTemp);
 }
 
 function getCurrentPosition(position) {
